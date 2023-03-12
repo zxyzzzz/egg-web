405b2ce0bccbe758ae1b9a1e2518e654e0af625e
xinxinyu <11919369+xinxinyuzzzz@user.noreply.gitee.com>
2023-03-12 16:17:20 +0800

    1

--- server.sh
+++ server.sh
@@ -4,6 +4,6 @@ cd /root/egg-web
 rm -rf /root/egg-web/app/public
 rm -rf /root/egg-web/node_modules
 cp -r /root/sofltware/node_modules /root/egg-web/
-cp -r /root/sofltware/vue-app-public/dist/* /root/egg-web/app/public
+cp -r /root/sofltware/vue-app-public/public/ /root/egg-web/app/public/
 npm run stop
 npm run start
