e3e8afac417a00554a2cda31f195fd2e29e3566d
xinxinyu <11919369+xinxinyuzzzz@user.noreply.gitee.com>
2023-03-12 15:45:24 +0800

    1

--- server.sh
+++ server.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd /root/egg-web
 # npm install
+cp -r /root/sofltware/vue-app-public /root/egg-web/app/public
 cp -r /root/sofltware/node_modules /root/egg-web/
 npm run stop
 npm run start
