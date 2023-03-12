f675250807ef3dabb19a47cb33f14847ee8c8d71
xinxinyu <11919369+xinxinyuzzzz@user.noreply.gitee.com>
2023-03-12 16:32:42 +0800

    1

--- server.sh
+++ server.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
+# 切换工作目录
 cd /root/egg-web
 # npm install
+# 删除文件夹
 rm -rf /root/egg-web/app/public
 rm -rf /root/egg-web/node_modules
+# 拷贝文件夹
 cp -r /root/sofltware/node_modules /root/egg-web/
 cp -r /root/sofltware/vue-app-public/public/ /root/egg-web/app/public/
+# 停止node服务
 npm run stop
+# 启动node服务
 npm run start
