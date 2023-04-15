040e5af024ce57a687a227bd4b621d2c6eaa2b07
zxyzzzz <2352157053@qq.com>
2023-04-15 11:50:54 +0800

    修改mysql地址

--- config/config.prod.js
+++ config/config.prod.js
@@ -20,7 +20,7 @@ module.exports = appInfo => {
     // 单数据库信息配置
     client: {
       // host
-      host: '127.0.0.1',
+      host: '101.42.46.158',
       // 端口号
       port: '3306',
       // 用户名
